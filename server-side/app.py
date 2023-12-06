77fed7467628f8793a08d454fca6aa9339695f67
Isaiah Murray <isaiahmurray@Isaiahs-MacBook-Pro.local>
2023-12-06 11:41:04 -0500

    progress

--- server-side/app.py
+++ server-side/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, redirect, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
 from cs50 import SQL
 
@@ -7,58 +7,50 @@ db = SQL("sqlite:///api-logs.db")
 
 @app.route('/')
 def home():
-    return render_template('index.html')
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        # Login logic handled in the api_login route
-        pass
-    else:
-        return render_template('login.html')
+    return render_template('options.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-        # Registration logic handled in the api_register route
-        pass
+        username = request.form['username']
+        password = request.form['password']
+
+        # Check if username already exists
+        existing_user = db.execute("SELECT * FROM users WHERE username = ?", username)
+        if existing_user:
+            error_message = "Username already exists."
+            return render_template('register.html', error=error_message)
+
+        # Hash the password and insert new user
+        password_hash = generate_password_hash(password)
+        db.execute("INSERT INTO users (username, password_hash) VALUES (?, ?)", username, password_hash)
+
+        # Retrieve user ID
+        user_id = db.execute("SELECT id FROM users WHERE username = ?", username)[0]['id']
+        return render_template('post_register.html', user_id=user_id)
     else:
-        return render_template('register.html')
+        return render_template('register.html', error='');
 
-@app.route('/api/register', methods=['POST'])
-def api_register():
-    username = request.json.get('username')
-    password = request.json.get('password')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
 
-    # Check if username already exists
-    existing_user = db.execute("SELECT * FROM users WHERE username = ?", username)
-    if existing_user:
-        return jsonify({"error": "Username already exists"}), 400
-
-    # Hash the password and insert new user
-    password_hash = generate_password_hash(password)
-    db.execute("INSERT INTO users (username, password_hash) VALUES (?, ?)", username, password_hash)
-
-    # After successful registration, retrieve user ID
-    user_id = db.execute("SELECT id FROM users WHERE username = ?", username)
-    return jsonify({"status": "registered", "user_id": user_id[0]["id"]})
-
-@app.route('/api/login', methods=['POST'])
-def api_login():
-    username = request.json.get('username')
-    password = request.json.get('password')
-
-    # Retrieve user by username
-    user = db.execute("SELECT * FROM users WHERE username = ?", username)
-
-    if user and check_password_hash(user[0]["password_hash"], password):
-        return jsonify({"status": "logged in", "user_id": user[0]["id"]})
+        # Retrieve user by username and check password
+        user = db.execute("SELECT * FROM users WHERE username = ?", username)
+        if user and check_password_hash(user[0]["password_hash"], password):
+            user_id = user[0]["id"]
+            return render_template('post_login.html', user_id=user_id)
+        else:
+            error_message = "Invalid username or password"
+            return render_template('login.html', error=error_message)
     else:
-        return jsonify({"error": "Invalid username or password"}), 401
+        return render_template('login.html')
 
-@app.route('/api/logout', methods=['POST'])
-def api_logout():
-    # Perform any necessary logout logic
+@app.route('/logout', methods=['POST'])
+def logout():
+    # Logic to handle logout
     return jsonify({"status": "logged out"})
 
 if __name__ == '__main__':
