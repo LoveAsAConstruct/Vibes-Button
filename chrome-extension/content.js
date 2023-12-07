c04fc9e28738809786d2fa7d1b14b7fb3ced1d28
Isaiah Murray <isaiahmurray@isaiahs-mbp.lan>
2023-12-06 22:02:21 -0500

    code formatting

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -1,52 +1,61 @@
+// Indicate that the content script is ready
 console.log("content online");
 window.contentScriptReady = true;
-window.addEventListener("message", function(event) {
-  console.log("message recieved");
 
-  // Only accept messages from the same frame
-  if (event.source !== window) return;
-  
-  if (event.data.type) {
-    console.log("Received event data:", event.data);
-    if (event.data.type === "SET_USER_ID") {
+// Handle messages received from the page
+window.addEventListener("message", function(event) {
+  // Ignore messages not from the same frame
+  if (event.source !== window) {
+    return;
+  }
+
+  console.log("message received");
+
+  // Process different types of messages
+  switch (event.data.type) {
+    case "SET_USER_ID":
       console.log("setting userid");
-      // Send message to background script to set user ID
+      // Send user ID to the background script
       chrome.runtime.sendMessage({ action: "setUserId", userId: event.data.userId });
-    } else if (event.data.type === "GET_USER_ID") {
+      break;
+
+    case "GET_USER_ID":
       console.log("getting userid");
-      // Send message to background script to get user ID
+      // Request user ID from the background script
       chrome.runtime.sendMessage({ action: "getUserId" }, function(response) {
-          window.postMessage({ type: "USER_ID_RESPONSE", userId: response.userId }, "*");
+        window.postMessage({ type: "USER_ID_RESPONSE", userId: response.userId }, "*");
       });
-    } else if (event.data.type === "FROM_PAGE") {
+      break;
+
+    case "FROM_PAGE":
       console.log("sending message to background script:", event.data.message);
+      // Relay message to the background script and post the response
       chrome.runtime.sendMessage(event.data.message, function(response) {
         window.postMessage({ type: "FROM_EXTENSION", message: response }, "*");
       });
-    }
+      break;
   }
 }, false);
 
+// Handle DOMContentLoaded event
 document.addEventListener('DOMContentLoaded', function() {
   // Check if the current page is the homepage
   if (window.location.href === 'http://127.0.0.1:5000/') {
-      chrome.runtime.sendMessage({ action: "getUserId" }, function(response) {
-          if (response && response.userId) {
-              // Send the user ID to the Flask application
-              fetch('http://localhost:5000/receive-user-id', {
-                  method: 'POST',
-                  headers: {
-                      'Content-Type': 'application/json'
-                  },
-                  body: JSON.stringify({ userId: response.userId })
-              })
-              .then(response => response.json())
-              .then(data => {
-                  // Process the response from the Flask application
-                  console.log('Received response from Flask:', data);
-              })
-              .catch(error => console.error('Error:', error));
-          }
-      });
+    chrome.runtime.sendMessage({ action: "getUserId" }, function(response) {
+      if (response && response.userId) {
+        // Send user ID to the Flask application
+        fetch('http://localhost:5000/receive-user-id', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ userId: response.userId })
+        })
+        .then(response => response.json())
+        .then(data => {
+          // Log the response from Flask
+          console.log('Received response from Flask:', data);
+        })
+        .catch(error => console.error('Error:', error));
+      }
+    });
   }
 });
--- chrome-extension/overlay.js
+++ chrome-extension/overlay.js
@@ -1,14 +1,18 @@
-// Add the CSS file to the head of the document
-
+// Create and append a link to the CSS file in the document head
 const link = document.createElement('link');
 link.href = chrome.runtime.getURL('overlay.css');
 link.type = 'text/css';
 link.rel = 'stylesheet';
 document.head.appendChild(link);
+
+// Log script loading and current URL
 console.log("Script loaded");
-console.log('Current URL:', window.location.href); // Get current page URL
-var url;
-var requesting = false;
+console.log('Current URL:', window.location.href);
+
+let url;
+let requesting = false;
+
+// Listen for messages from the runtime
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.url) {
         console.log("Current URL:", request.url);
@@ -16,125 +20,89 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     }
 });
 
+// Function to remove special characters from a string
 function removeSpecialCharacters(inputString) {
-    // Define a regular expression to match special characters
-    const specialCharRegex = /[^a-zA-Z0-9\s.,?!'"():;]/g;
-  
-    // Use the replace() method to remove special characters
-    const cleanedString = inputString.replace(specialCharRegex, '');
-  
-    return cleanedString;
-  }
+    return inputString.replace(/[^a-zA-Z0-9\s.,?!'"():;]/g, '');
+}
 
+// Load the HTML overlay and append to the body
 fetch(chrome.runtime.getURL('overlay.html'))
-  .then(response => response.text())
-  .then(data => {
-    const div = document.createElement('div');
-    div.innerHTML = data;
-    document.body.appendChild(div);
+    .then(response => response.text())
+    .then(data => {
+        const div = document.createElement('div');
+        div.innerHTML = data;
+        document.body.appendChild(div);
 
-    
-    const collapseButton = document.getElementById('collapse-button');
-    const expandButton = document.getElementById('expand-button');
-    const popupContent = document.getElementById('button-container');
-    const activationButton = document.getElementById('activation-button');
-    const textBox = document.getElementById('text-box');
-    const hoverButton = document.getElementById('hover-button');
+        const collapseButton = document.getElementById('collapse-button');
+        const expandButton = document.getElementById('expand-button');
+        const popupContent = document.getElementById('button-container');
+        const activationButton = document.getElementById('activation-button');
+        const textBox = document.getElementById('text-box');
 
-    
+        // Event listener for the hover button
+        document.getElementById('hover-button').addEventListener("click", () => {
+            console.log("Hover button clicked!");
+        });
 
-    // Add a click event listener for the hover button
-    hoverButton.addEventListener("click", function () {
-        // Handle the click event of the hover button here
-        console.log("Hover button clicked!");
-    });
-
-    function revealText(text, object) {
-        let index = 0;
-        const interval = 10; // Interval between revealing each character (in milliseconds)
-    
-        function revealCharacter() {
-            if (index < text.length) {
-                object.textContent += text[index];
-                index++;
-                setTimeout(revealCharacter, interval);
+        // Function to reveal text character by character
+        function revealText(text, object) {
+            let index = 0;
+            const interval = 10;
+            function revealCharacter() {
+                if (index < text.length) {
+                    object.textContent += text[index++];
+                    setTimeout(revealCharacter, interval);
+                }
             }
+            revealCharacter();
         }
-    
-        revealCharacter();
-    }
-    textBox.addEventListener('click', function() {
-        textBox.innerText='';
-    });
-    collapseButton.addEventListener('click', function() {
-        popupContent.style.display = 'none';
-        textBox.style.display = 'none';  // Hide the text box
-        expandButton.style.display = 'block';
-    });
 
-    expandButton.addEventListener('click', function() {
-        popupContent.style.display = 'block';
-        textBox.style.display = 'inline-block';  // Show the text box (or 'block')
-        expandButton.style.display = 'none';
-    });
-    activationButton.addEventListener('click', function() {
-        if(!requesting){
+        // Event listeners for various buttons
+        textBox.addEventListener('click', () => textBox.innerText = '');
+        collapseButton.addEventListener('click', () => {
+            popupContent.style.display = 'none';
+            textBox.style.display = 'none';
+            expandButton.style.display = 'block';
+        });
+        expandButton.addEventListener('click', () => {
+            popupContent.style.display = 'block';
+            textBox.style.display = 'inline-block';
+            expandButton.style.display = 'none';
+        });
+
+        // Event listener for the activation button
+        activationButton.addEventListener('click', () => {
+            if (requesting) return;
             requesting = true;
-            // Get the current URL
-            var currentUrl = window.location.href;
-            
-            // Find the text box element
-            var textBox = document.getElementById('text-box');
-            
-            // Set the text box content to the current URL
-            if (textBox) {
-                console.log("url set")
-                textBox.textContent = currentUrl +"...";
-            } else {
-                console.error('Text box element not found');
-            }
+
+            const currentUrl = window.location.href;
+            textBox.textContent = currentUrl + "...";
             activationButton.classList.add('loading');
-            chrome.runtime.sendMessage({
-                contentScriptQuery: "queryChatGPT",
-                url: currentUrl
-            }, response => {
-                // Check if textBox exists
+
+            chrome.runtime.sendMessage({ contentScriptQuery: "queryChatGPT", url: currentUrl }, response => {
                 if (!textBox) {
                     console.error('Text box element not found');
-                    requesting=false;
+                    requesting = false;
                     return;
                 }
-            
-                // Check for response and any errors
                 if (response) {
                     activationButton.classList.remove('loading');
                     if (response.reply) {
-                        // Use the reply from the background script
-                        console.log(response);
-                        response_chunks = response.reply.split(/\[\[(.*?)\]\]/).filter(Boolean);
-                        response_chunks.sort((a, b) => b.length - a.length);
+                        const responseChunks = response.reply.split(/\[\[(.*?)\]\]/).filter(Boolean);
+                        responseChunks.sort((a, b) => b.length - a.length);
                         textBox.innerText = '';
-                        revealText(removeSpecialCharacters(response_chunks[0]), textBox);
+                        revealText(removeSpecialCharacters(responseChunks[0]), textBox);
                     } else if (response.error) {
-                        // Handle any error sent from the background script
                         console.error('Error from background script:', response.error);
                         textBox.textContent = 'Error: ' + response.error;
                     }
                 } else {
-                    myButton.classList.remove('loading');
-                    // Handle the case where response is undefined
+                    activationButton.classList.remove('loading');
                     console.error('No response received from background script');
                     textBox.textContent = 'No response received';
                 }
-                requesting=false;        
+                requesting = false;        
             });
-        }
-        else{
-            return;
-        }
-    });
+        });
     })
-    
-  .catch(err => console.error('Error loading the overlay:', err));
-
-  
+    .catch(err => console.error('Error loading the overlay:', err));
